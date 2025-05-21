d251a9e5f6a94d7a4e973501148daca5a151a154
Ankesh Suman <suman.ankesh@gmail.com>
2025-05-21 22:11:03 +0530

    main updated

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # codebasics ML course: codebasics.io, all rights reserverd
 
 import streamlit as st
-import numpy as np
+# import numpy as np
 from prediction_helper import predict
 
 # Define the page layout
